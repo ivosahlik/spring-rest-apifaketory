e242fab187ef464e7ba79429869f9ec260e2aad4
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-04-09 21:44:04 +0200

    add to @PropertySource classpath, unit test apiservice

--- src/main/java/cz/ivosahlik/springrestapifaketory/SpringRestApifaketoryApplication.java
+++ src/main/java/cz/ivosahlik/springrestapifaketory/SpringRestApifaketoryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("/config/config.properties")
+@PropertySource("classpath:/config/config.properties")
 @SpringBootApplication
 public class SpringRestApifaketoryApplication {
 
---
+++ src/test/java/cz/ivosahlik/springrestapifaketory/service/impl/ApiServiceImplTest.java
@@ -0,0 +1,39 @@
+package cz.ivosahlik.springrestapifaketory.service.impl;
+
+import cz.ivosahlik.springrestapifaketory.api.User;
+import cz.ivosahlik.springrestapifaketory.service.ApiService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Intellij Idea
+ * Created by ivosahlik on 09/04/2018
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiServiceImplTest {
+
+    @Autowired
+    ApiService apiService;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getUsers() {
+        List<User> list = apiService.getUsers(3);
+        assertEquals(4, list.size());
+    }
+}
